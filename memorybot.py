3d78bd8cd0882f390754b6177832251f8b59a09a
PatrickVGould <125094430+PatrickVGould@users.noreply.github.com>
2023-04-10 14:16:38 +1000

    Update memorybot.py

--- memorybot.py
+++ memorybot.py
@@ -28,8 +28,9 @@ from langchain.utilities import GoogleSearchAPIWrapper
 
 API_O = st.secrets["OPENAI_API_KEY"]
 
-llm_math = ChatOpenAI(temperature=0,
-            openai_api_key=API_O, 
+llm_math = OpenAI(temperature=0,
+            openai_api_key=API_O,
+            model_name='gpt-3.5-turbo', 
             verbose=False) 
 
 llm_math_chain = LLMMathChain(llm=llm_math, verbose=False)
@@ -149,7 +150,7 @@ prompt = ZeroShotAgent.create_prompt(
 )
 st.session_state.memory = ConversationBufferMemory(memory_key="chat_history")
 
-llm_chain = LLMChain(llm=ChatOpenAI(temperature=0.5, openai_api_key= st.secrets["OPENAI_API_KEY"]), prompt=prompt)
+llm_chain = LLMChain(llm=OpenAI(temperature=0.5, model_name='gpt-3.5-turbo', openai_api_key= st.secrets["OPENAI_API_KEY"]), prompt=prompt)
 agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=False)
 agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=False, memory=st.session_state.memory)
 
